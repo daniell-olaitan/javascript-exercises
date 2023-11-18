7488344202e514e28531e172d5febcdee259a02b
daniell-olaitan <daniell.olaitan@gmail.com>
2023-11-18 11:59:41 +0100

    Complete the 06_leapYears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const leapYears = function (year) {
+    let remainder;
+    let quotient;
+    let result;
+    
+    remainder = year % 100;
+    quotient = Math.floor(year / 100); 
+    if (remainder === 0) {
+        result = quotient % 4 === 0 ? true : false;
+    } else {
+        result = year % 4 === 0 ? true : false;
+    }
 
+    return result;
 };
 
 // Do not edit below this line
