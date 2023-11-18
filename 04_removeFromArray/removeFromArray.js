feca7eceb06d3c38f89a621dd22b241677642a6f
daniell-olaitan <daniell.olaitan@gmail.com>
2023-11-18 11:10:42 +0100

    Complete the 04_removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removes) {
+    let result = [];
+    let removeTag = false;
+    
+    for (let element of array) {
+        for (let remove of removes) {
+            if (remove === element) {
+                removeTag = true;
+                break;
+            }
+        }
 
+        if (!removeTag) {
+            result.push(element);
+        } else {
+            removeTag = false;
+        }
+    }
+
+    return result;
 };
 
 // Do not edit below this line
