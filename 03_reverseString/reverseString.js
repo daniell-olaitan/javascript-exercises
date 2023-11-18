0a4f467924e696fc625287a42a97924b716be543
daniell-olaitan <daniell.olaitan@gmail.com>
2023-11-18 10:40:05 +0100

    Complete the 03_reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let index = string.length - 1;
+    let result = "";
 
+    for (; index >= 0; --index) {
+        result = result + string[index];
+    }
+
+    return result;
 };
 
 // Do not edit below this line
