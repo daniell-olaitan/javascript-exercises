94e314dcc206507f89c478a9f3579359dc072796
daniell-olaitan <daniell.olaitan@gmail.com>
2023-11-23 10:47:24 +0100

    Complete the 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,30 @@
-const palindromes = function () {
+function palindrome(text) {
+    while (text) {
+        if (text.at(0).toLowerCase() !== text.at(-1).toLowerCase()) {
+            return false;
+        }
 
+        text = text.slice(1, -1);
+    }
+
+    return true;
+}
+
+const palindromes = function (string) {
+    let text = '';
+
+    if (string.length <= 1) {
+        return true;
+    }
+
+    for (let char of string) {
+        if ((char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') ||
+            char >= '0' && char <= '9') {
+            text += char;
+        }
+    }
+
+    return palindrome(text);
 };
 
 // Do not edit below this line
