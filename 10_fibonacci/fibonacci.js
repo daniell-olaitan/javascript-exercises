536b2a057b9b975ac3006de019b3a67d2ca05faa
daniell-olaitan <daniell.olaitan@gmail.com>
2023-11-23 11:03:52 +0100

    Complete the 10_fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let firstNumber = 1;
+    let secondNumber = 1;
+    
+    number = parseInt(number);
+    if (number < 0) {
+        return 'OOPS';
+    }
 
+    if (number === 1 || number === 2) {
+        return 1;
+    }
+
+    for (let i = 3; i <= number; i++) {
+        [firstNumber, secondNumber] = [secondNumber, firstNumber + secondNumber];
+    }
+    
+    console.log(secondNumber);
+    return secondNumber;
 };
 
 // Do not edit below this line
